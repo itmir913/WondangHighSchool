2a5103ce4571315a0d6ba3f82ae703f461de7a8a
whdghks913 <whdghks913@naver.com>
2015-12-11 01:31:44 +0900

    TimeTable Widget bug fix

--- app/src/main/java/wondang/icehs/kr/whdghks913/wondanghighschool/widget/WidgetBroadCast.java
+++ app/src/main/java/wondang/icehs/kr/whdghks913/wondanghighschool/widget/WidgetBroadCast.java
@@ -15,6 +15,7 @@ public class WidgetBroadCast extends BroadcastReceiver {
         String ACTION = mIntent.getAction();
 
         BapWidget.updateAllBapWidget(mContext);
+        TimeTableWidget.updateAllTimeTableWidget(mContext);
 
         if (Intent.ACTION_BOOT_COMPLETED.equals(ACTION)) {
             // 24시간마다 앱 위젯 업데이트하기
@@ -23,7 +24,7 @@ public class WidgetBroadCast extends BroadcastReceiver {
 
             Intent mIntentDate = new Intent(mContext, WidgetBroadCast.class);
             PendingIntent mPending = PendingIntent.getBroadcast(mContext, 0, mIntentDate, 0);
-            mCalendar.set(mCalendar.get(Calendar.YEAR), mCalendar.get(Calendar.MONTH), mCalendar.get(Calendar.DAY_OF_MONTH) + 1, 0, 0);
+            mCalendar.set(mCalendar.get(Calendar.YEAR), mCalendar.get(Calendar.MONTH), mCalendar.get(Calendar.DAY_OF_MONTH) + 1, 1, 0);
             mAlarm.setRepeating(AlarmManager.RTC_WAKEUP, mCalendar.getTimeInMillis(), 24 * 60 * 60 * 1000, mPending);
         }
     }
